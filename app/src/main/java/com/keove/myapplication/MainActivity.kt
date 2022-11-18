ac4e3b289938b898d44a59b3d047d999b26d29ea
Cansu-Kose <cansuu23977@gmail.com>
2022-11-18 16:27:54 +0300

    Initial commit

---
+++ app/src/main/java/com/keove/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.keove.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun project(){
+        Log.v("Cansu","Hello World")
+    }
+
+}
